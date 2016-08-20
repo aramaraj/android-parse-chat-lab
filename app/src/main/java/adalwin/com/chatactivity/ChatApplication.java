14e68a8cd4dddaf984491283e5b862e242799e9d
Ashok Ramaraj - aramar1 <aramar1@L-SNV5EDJDF8-M.local>
2016-08-20 14:21:17 -0700

    Firs commit

---
+++ app/src/main/java/adalwin/com/chatactivity/ChatApplication.java
@@ -0,0 +1,35 @@
+package adalwin.com.chatactivity;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.interceptors.ParseLogInterceptor;
+
+import adalwin.com.chatactivity.model.Message;
+
+/**
+ * Created by aramar1 on 6/2/16.
+ */
+public class ChatApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Register your parse models here
+        ParseObject.registerSubclass(Message.class);
+        // Existing initialization happens after all classes are registered
+        // For open-source Parse backend
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("classconnect") // should correspond to APP_ID env variable
+                .addNetworkInterceptor(new ParseLogInterceptor())
+                .server("https://classconnect.herokuapp.com/parse/").build());
+        /*Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("93bd0b59a90d46b1999e484431b83f41") // should correspond to APP_ID env variable
+                .addNetworkInterceptor(new ParseLogInterceptor())
+                .server("https://simplechatclient.herokuapp.com/parse/").build());*/
+    }
+}
+
+
+
+
